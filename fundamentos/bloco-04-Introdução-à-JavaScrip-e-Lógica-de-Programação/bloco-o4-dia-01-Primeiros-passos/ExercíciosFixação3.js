606c42848faa9a7e65c32dcba0a58f80e5294eea
IgorSouzaDeOliveira <igorsouzaoliveira002@gmail.com>
2021-11-29 17:41:09 -0300

    exercicio fixaçao 3

---
+++ fundamentos/bloco-04-Introdução-à-JavaScrip-e-Lógica-de-Programação/bloco-o4-dia-01-Primeiros-passos/ExercíciosFixação3.js
@@ -0,0 +1,10 @@
+const notaDesafioPraticoAna = 60;
+if(notaDesafioPraticoAna >= 80){
+    console.log("Aprovado");
+}
+else if(notaDesafioPraticoAna < 80 && notaDesafioPraticoAna >= 60){
+    console.log("Você está na nossa lista de espera");
+}
+else if(notaDesafioPraticoAna < 60){
+    console.log("Você foi reprovada(o)");
+}
