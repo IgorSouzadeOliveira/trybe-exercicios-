6319ad2969458975b3d4cea917cb6b8a9c19d422
IgorSouzaDeOliveira <igorsouzaoliveira002@gmail.com>
2021-11-30 16:16:59 -0300

    mais alguns exercicios realizados

--- fundamentos/bloco-04-Introdução-à-JavaScrip-e-Lógica-de-Programação/bloco-o4-dia-01-Primeiros-passos/Exercicios-dia-1/Exercício-dia-01.js
+++ fundamentos/bloco-04-Introdução-à-JavaScrip-e-Lógica-de-Programação/bloco-o4-dia-01-Primeiros-passos/Exercicios-dia-1/Exercício-dia-01.js
@@ -28,5 +28,32 @@ else if (number3 > number1 && number3 > number2){
     console.log("number3 maior número")
 } */
 
+/* const number = -1;
+if(number > 0){
+    console.log("positive");
+}
+else if (number < 0){
+    console.log("negative");
+}else{
+    console.log("zero");
+}
+ */
 
+/* const valor1 = 80;
+const valor2 = 70;
+const valor3 = 30;
+if(valor1 + valor2 + valor3 === 180){
+    console.log(true);
+}else{
+    console.log(false);
+}else{
+    console.log('Erro: ângulo inválido');
+  } */
 
+ /*  let peçaDeXadrez = "Rei";
+  if(peçaDeXadrez.toLowerCase() === "rei"){
+      console.log("rei -> diagonal");
+  }else{
+      console.log("peça inválida");
+  }
+ */
