5fd02973c2ae0444caf55ab264c332907d4cea6c
IgorSouzaDeOliveira <igorsouzaoliveira002@gmail.com>
2021-11-30 14:58:26 -0300

    1,2,3

---
+++ fundamentos/bloco-04-Introdução-à-JavaScrip-e-Lógica-de-Programação/bloco-o4-dia-01-Primeiros-passos/Exercicios-dia-1/Exercício-dia-01.js
@@ -0,0 +1,32 @@
+/* const a = 6;
+const b = 66;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b); */
+
+/* const number1 = 40;
+const number2 = 30;
+if(number1 > number2){
+    console.log("number1 maior número");  
+}
+else if(number2 > number1){
+    console.log("number2 maior número");
+} */
+
+/* const number1 = 60;
+const number2 = 70;
+const number3 = 90;
+if(number1 > number2 && number1 > number3){
+    console.log("number1 maior número");  
+}
+else if(number2 > number1 && number2 > number3){
+    console.log("number2 maior número");
+}
+else if (number3 > number1 && number3 > number2){
+    console.log("number3 maior número")
+} */
+
+
+
